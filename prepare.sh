256b38e69b58e882b7230877d3f91e0bffa9dc33
BackstreetCultist <charlie@101a.co.uk>
2022-01-13 13:06:31 +0000

    fixup! Initial commit - add directory structure and runners from prototype

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,4 @@
 cd src/main/java
-# cp ~/.m2/repository/org/bytedeco/javacpp/1.5.6/javacpp-1.5.6.jar javacpp.jar
 curl -O https://repo1.maven.org/maven2/org/bytedeco/javacpp/1.5.6/javacpp-1.5.6.jar
 mv javacpp-1.5.6.jar javacpp.jar
 javac -cp javacpp.jar tech/charliewilkins/prototype/CppExposedHeuristic.java tech/charliewilkins/prototype/**/*.java
