fac5e6015122fe2c2456352578391d57095eaceb
Charles Wilkins <psychw@exmail.nottingham.ac.uk>
2022-01-31 13:35:48 +0000

    Add application tests
    This adds tests for the applyPopulation function. Note that no tests
    are added for the applyHeuristic function, as this is merely a
    wrapper around the Java call, and contains no program logic of its own
    
    Reference: HYH-002(?)

--- src/test/Haskell/tech/charliewilkins/hyfi/HyperHeuristicTest.hs
+++ src/test/Haskell/tech/charliewilkins/hyfi/HyperHeuristicTest.hs
@@ -71,3 +71,30 @@ checkRandomiseListLength = quickCheck (prop_randomiseList_len :: Int -> [Char] -
 -- AND the list contains the same objects
 prop_randomiseList_objects seed xs = sort (randomiseList seed xs) == sort xs
 checkRandomiseListObjects = quickCheck (prop_randomiseList_objects :: Int -> [Char] -> Bool)
+
+-- APPLICATION ----------------------------------------------------------------
+checkApplication = do
+    -- javacpp_init
+    checkApplyPopulation
+
+--applyPopulation
+-- GIVEN a non-empty heuristic population
+-- WHEN the population is applied
+checkApplyPopulation = do
+    checkApplyPopulationLength
+    checkApplyPopulationRounds
+    checkApplyPopulationHeuristics
+
+-- THEN the resulting population is of the same length
+prop_applyPopulation_len (NonEmpty hs) = length (applyPopulation hs) == length hs
+checkApplyPopulationLength = quickCheck prop_applyPopulation_len
+
+-- AND each heuristic's round counter has increased by one
+prop_applyPopulation_rounds seed (NonEmpty hs) = snd (snd ((applyPopulation hs) !! i)) == (snd (snd (hs !! i))) + 1
+    where i = getRandomIndex seed hs
+checkApplyPopulationRounds = quickCheck prop_applyPopulation_rounds
+
+-- AND the heuristics have not been modified
+prop_applyPopulation_heuristics seed (NonEmpty hs) = fst ((applyPopulation hs) !! i) == fst (hs !! i)
+    where i = getRandomIndex seed hs
+checkApplyPopulationHeuristics = quickCheck prop_applyPopulation_heuristics
