75854ab90ae5122c3f5894414689a9b6464c49da
BackstreetCultist <charlie@101a.co.uk>
2022-02-22 14:27:30 +0000

    WIP - add ability to apply a heuristic to a population of solution

--- src/main/Haskell/tech/charliewilkins/hyfi/HyperHeuristic/Functions/Application.hs
+++ src/main/Haskell/tech/charliewilkins/hyfi/HyperHeuristic/Functions/Application.hs
@@ -1,13 +1,28 @@
 module Application where
 
+import Control.Monad.State
+
 import HyperHeuristicTypes
 import RandomOperators (heuristicToSeed)
+import Solution (applyHeuristicRepresentationToPopulation, SolutionPopulation)
 
-applyPopulation :: HeuristicPopulation -> HeuristicPopulation
-applyPopulation hs = [(h, ((s + (applyHeuristic h)), (r+1))) | (h, (s, r)) <- hs]
+applyPopulation :: (HeuristicPopulation, SolutionPopulation) -> (HeuristicPopulation, SolutionPopulation)
+-- -- applyPopulation (hs, ss) = [(h, ((s + (applyHeuristic h)), (r+1))) | (h, (s, r)) <- hs]
+applyPopulation ([], ss) = ([],ss)
+-- applyPopulation ((h, (s,r):hs, ss) = ((h, ((s + s'), (r+1))), ss') : applyPopulation (hs,ss')
+applyPopulation ((h, (s,r)):hs, ss) = ((h, ((s + s'), (r+1))) : hs', ss'')
+                            where
+                                (hs', ss'') = applyPopulation (hs,ss')
+                                (ss', s') = applyHeuristic h ss
 
 -- apply a heuristic and get back a score for that run
 -- this is where we call in to the solution layer
-applyHeuristic :: Heuristic -> Int
-applyHeuristic =  heuristicToSeed
--- TODO replace with call to solution layer
+-- the score is calculated as the average of the scores recieved from that layer
+applyHeuristic :: Heuristic -> SolutionPopulation -> (SolutionPopulation, Int)
+applyHeuristic h ss =  (fst x, avg (snd x))
+                    where
+                        x = unzip (applyHeuristicRepresentationToPopulation h ss)
+
+avg :: [Int] -> Int
+avg [] = 0
+avg xs = (sum xs) `div` (length xs)
