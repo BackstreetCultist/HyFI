8de6ac0fac805661f1dc29e8c0c0258a0c055660
BackstreetCultist <charlie@101a.co.uk>
2022-02-25 14:55:47 +0000

    WIP - remove unecessary code from HyperHeuristic.hs

--- src/main/Haskell/tech/charliewilkins/hyfi/HyperHeuristic/HyperHeuristic.hs
+++ src/main/Haskell/tech/charliewilkins/hyfi/HyperHeuristic/HyperHeuristic.hs
@@ -17,26 +17,3 @@ generateHeuristic seed = take 16 (randomRs ('0', '1') (mkStdGen seed))
 generateHeuristicPopulationOfSize :: Int -> Int -> HeuristicPopulation
 generateHeuristicPopulationOfSize 0 _ = []
 generateHeuristicPopulationOfSize n seed = (generateHeuristic seed, (0, 0)) : generateHeuristicPopulationOfSize (n-1) (seed+1)
-
--- CONTROL --------------------------------------------------------------------
-
-run :: Int -> NominalDiffTime -> IO ()
-run seed limit = do
-    -- javacpp_init
-    let hPop = generateHeuristicPopulationOfSize 8 seed
-    startTime <- getCurrentTime
-    print hPop
-    currentTime <- getCurrentTime
-    print("Starting loop")
-    newPop <- coreLoop hPop startTime currentTime limit
-    print("Exited loop")
-    print newPop
-
-coreLoop :: HeuristicPopulation -> UTCTime -> UTCTime -> NominalDiffTime -> IO (HeuristicPopulation)
-coreLoop hPop startTime currentTime limit | ((diffUTCTime currentTime startTime) <= limit) = do
-                                                                                        newTime <- getCurrentTime
-                                                                                        let newPop = evolvePopulation (applyPopulation hPop)
-                                                                                        print newPop
-                                                                                        coreLoop newPop startTime newTime limit
-                                          | otherwise = do
-                                                return hPop
