06e4e7e72a2d1dd55a03648e73be4c08bd7f5d1e
BackstreetCultist <charlie@101a.co.uk>
2022-03-01 14:49:23 +0000

    fixup! Add random bit flip operator for MAX-SAT

--- src/main/Haskell/tech/charliewilkins/hyfi/Solution/BooleanSatisfiability/Functions/BooleanSatisfiabilityOperators.hs
+++ src/main/Haskell/tech/charliewilkins/hyfi/Solution/BooleanSatisfiability/Functions/BooleanSatisfiabilityOperators.hs
@@ -11,10 +11,3 @@ flipRandomVariable s m inst = flipRandomVariable (take i s ++ [bit] ++ drop (i+1
                                     bit = if (s !! i) == True then False else True
                                     i = getRandomIndex getSeed' s
 --TODO having no seed besides the time means that you only change the same var each time
-
-flipRandomVariableFromBrokenClause :: Operator
-flipRandomVariableFromBrokenClause s 0 _ = s
-flipRandomVariableFromBrokenClause s m inst = flipRandomVariableFromBrokenClause (take i s ++ [bit] ++ drop (i+1) s) (m-1) inst
-                                        where
-                                            bit = if (s !! i) == True then False else True
-                                            i = getRandomIndex getSeed' s
