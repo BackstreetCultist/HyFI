39be8dd101d86cbf054a17941b2ec46555f65b14
BackstreetCultist <charlie@101a.co.uk>
2022-03-03 13:13:22 +0000

    Add getRandomIndexFromBrokenClause operator

--- src/main/Haskell/tech/charliewilkins/hyfi/Solution/BooleanSatisfiability/Functions/BooleanSatisfiabilityOperators.hs
+++ src/main/Haskell/tech/charliewilkins/hyfi/Solution/BooleanSatisfiability/Functions/BooleanSatisfiabilityOperators.hs
@@ -1,6 +1,7 @@
 module BooleanSatisfiabilityOperators where
 
 import RandomOperators (getSeed, getRandomIndex)
+import BooleanSatisfiabilityEvaluators (evaluateClause)
 import BooleanSatisfiabilityTypes
 
 -- LOCAL SEARCH HEURISTICS ----------------------------------------------------
@@ -11,3 +12,25 @@ flipRandomVariable s m inst = flipRandomVariable (take i s ++ [bit] ++ drop (i+1
                                     bit = if (s !! i) == '0' then '1' else '0'
                                     i = getRandomIndex (getSeed s) s
 --TODO having no seed besides the time means that you only change the same var each time
+
+-- Same as above but the variable (i+1) must be in a clause that is broken
+flipRandomVariableFromBrokenClause :: Operator
+flipRandomVariableFromBrokenClause s 0 _ = s
+flipRandomVariableFromBrokenClause s m inst = flipRandomVariableFromBrokenClause (take i s ++ [bit] ++ drop (i+1) s) (m-1) inst
+                                            where
+                                                bit = if (s !! i) == '0' then '1' else '0'
+                                                i = getRandomIndexFromBrokenClause (getSeed s) s inst
+
+getRandomIndexFromBrokenClause :: Int -> Solution -> Instance -> Int
+getRandomIndexFromBrokenClause seed s inst = if inBrokenClause (i+1) s inst then i else getRandomIndexFromBrokenClause (seed+1) s inst
+                                            where
+                                                i = getRandomIndex seed s
+
+inBrokenClause :: Int -> Solution -> Instance -> Bool
+inBrokenClause var s (_, clauses) = if length reducedClauses == sum (map (evaluateClause s) reducedClauses) then False else True
+                                    where
+                                        reducedClauses = reduceClauses var clauses
+
+--Remove all entries in list that i does not appear in
+reduceClauses :: Int -> [[Int]] -> [[Int]]
+reduceClauses i xs = [x | x <- xs, elem i x]
