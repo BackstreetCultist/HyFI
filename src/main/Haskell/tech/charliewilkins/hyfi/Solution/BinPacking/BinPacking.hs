5028de10b36a275a1b400713db8e90c378613aa4
BackstreetCultist <charlie@101a.co.uk>
2022-03-11 10:25:16 +0000

    Add generator for bin-packing problem

--- src/main/Haskell/tech/charliewilkins/hyfi/Solution/BinPacking/BinPacking.hs
+++ src/main/Haskell/tech/charliewilkins/hyfi/Solution/BinPacking/BinPacking.hs
@@ -3,9 +3,20 @@ module Solution.BinPacking.BinPacking where
 import System.IO
 import System.IO.Unsafe (unsafePerformIO)
 
+import HyperHeuristic.Functions.Helpers.RandomOperators (randomiseList)
 import Solution.BinPacking.Types.BinPackingTypes
 
--- generator :: Int -> Instance -> Solution
+generator :: Int -> Instance -> Solution
+generator seed i = firstFit [[]] (randomiseList seed (take (length (snd i)) [0..])) i
+
+firstFit :: Solution -> [Int] -> Instance -> Solution
+firstFit s [] i = s
+firstFit s (x:xs) i = firstFit (placeVal s x i ) xs i
+
+placeVal :: Solution -> Int -> Instance -> Solution
+placeVal [] y _ = [[y]]
+placeVal (x:xs) y i | ((sum (map (\z -> (snd i) !! z) x)) + ((snd i) !! y)) > (fst i) = x : placeVal xs y i
+                    | otherwise = (x ++ [y]) : xs
 
 getProblemInstance :: String -> Instance
 getProblemInstance file = unsafePerformIO (loadProblemInstance file)
