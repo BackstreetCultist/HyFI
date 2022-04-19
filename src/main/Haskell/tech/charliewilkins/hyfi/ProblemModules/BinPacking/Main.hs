27ce7c8a4109a1443a771a9121623a60c1b4a0aa
Charlie Wilkins <charlie@101a.co.uk>
2022-04-19 18:47:01 +0100

    Move to Next Fit for initial solution generation

--- src/main/Haskell/tech/charliewilkins/hyfi/ProblemModules/BinPacking/Main.hs
+++ src/main/Haskell/tech/charliewilkins/hyfi/ProblemModules/BinPacking/Main.hs
@@ -5,12 +5,12 @@ import System.IO.Unsafe (unsafePerformIO)
 
 import Helpers.RandomOperators (randomiseList)
 import ProblemModules.BinPacking.Types
-import ProblemModules.BinPacking.Functions.Acceptors (naiveAcceptor, improvingAcceptor, improvingOrChanceAcceptor, averageFullnessImprovingAcceptor, fullBinsImprovingAcceptor)
+import ProblemModules.BinPacking.Functions.Acceptors (improvingAcceptor, improvingOrChanceAcceptor, averageFullnessImprovingAcceptor, fullBinsImprovingAcceptor)
 import ProblemModules.BinPacking.Functions.Evaluators (newObjectiveValue, improvement, fullBinsImprovement, searchSpaceDistance, averageFullnessIncrease)
 import ProblemModules.BinPacking.Functions.Operators (randomPlace, maximisingPlace, repackLowestFilled, destroyHighestBins, destroyLowestBins, destroyRandomBins)
 
 generator :: Int -> Instance -> Solution
-generator seed i = firstFit [[]] (randomiseList seed (take (length (snd i)) [0..])) i
+generator seed i = nextFit [[]] (randomiseList seed (take (length (snd i)) [0..])) i
 
 firstFit :: Solution -> [Int] -> Instance -> Solution
 firstFit s [] i = s
@@ -20,6 +20,12 @@ placeVal :: Solution -> Int -> Instance -> Solution
 placeVal [] y _ = [[y]]
 placeVal (x:xs) y i | ((sum (map (\z -> (snd i) !! z) x)) + ((snd i) !! y)) > (fst i) = x : placeVal xs y i
                     | otherwise = (x ++ [y]) : xs
+
+nextFit :: Solution -> [Int] -> Instance -> Solution
+nextFit s [] _ = s
+nextFit [[]] (x:xs) i = nextFit [[x]] xs i
+nextFit s (x:xs) i | ((sum (map (\z -> (snd i) !! z) (last s))) + ((snd i) !! x)) > (fst i) = nextFit (s ++ [[x]]) xs i
+                   | otherwise = nextFit (reverse (tail (reverse s)) ++ [(last s) ++ [x]]) xs i
 
 getProblemInstance :: String -> Instance
 getProblemInstance file = unsafePerformIO (loadProblemInstance file)
@@ -38,13 +44,12 @@ loadProblemInstance file = do
 
 getOperatorsByClass :: [[Operator]]
 getOperatorsByClass = [[randomPlace, maximisingPlace], [repackLowestFilled], [destroyHighestBins, destroyLowestBins, destroyRandomBins]]
--- getOperatorsByClass = [[repackLowestFilled]]
 
 getEvaluators :: [Evaluator]
 getEvaluators = [improvement, fullBinsImprovement, searchSpaceDistance, averageFullnessIncrease]
 
 getAcceptors :: [Acceptor]
-getAcceptors = [naiveAcceptor, improvingAcceptor, improvingOrChanceAcceptor, averageFullnessImprovingAcceptor, fullBinsImprovingAcceptor]
+getAcceptors = [improvingAcceptor, improvingOrChanceAcceptor, averageFullnessImprovingAcceptor, fullBinsImprovingAcceptor]
 
 getObjectiveValue :: Evaluator
 getObjectiveValue = newObjectiveValue
