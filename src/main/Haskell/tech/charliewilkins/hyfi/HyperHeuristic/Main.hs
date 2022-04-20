d3a24e4c509d24891dafd4555bc1bc3113d5731c
Charlie Wilkins <charlie@101a.co.uk>
2022-04-20 10:10:25 +0100

    Code Cleanup - Hyper-Heuristic Layer

--- src/main/Haskell/tech/charliewilkins/hyfi/HyperHeuristic/Main.hs
+++ src/main/Haskell/tech/charliewilkins/hyfi/HyperHeuristic/Main.hs
@@ -37,15 +37,11 @@ reproductionStep hPop = generateChildren (selectParents hPop)
 
 selectParents :: HeuristicPopulation -> (Heuristic, Heuristic)
 selectParents hPop = tournamentSelection hPop (((length hPop) `div` 2) -1)
--- This assumes two parents - should we generalise to crossover between n parents?
-
-naiveSelection :: HeuristicPopulation -> (Heuristic, Heuristic)
-naiveSelection hPop = (fst (head sortedPop), fst (sortedPop !! 1)) where sortedPop = sortByAverageScore hPop
--- Naive selection - take two best-performing
 
 -- This implementation of tournament selection based on:
 -- https://towardsdatascience.com/genetic-algorithm-a-simple-and-intuitive-guide-51c04cc1f9ed
 -- But with average score rather than f(x)
+-- This also always chooses two different parents
 tournamentSelection :: HeuristicPopulation -> Int -> (Heuristic, Heuristic)
 tournamentSelection hPop k = 
                             (
@@ -59,13 +55,11 @@ getRandomSublistOfSize :: Int -> Int -> [a] -> [a]
 getRandomSublistOfSize 0 _ _ = []
 getRandomSublistOfSize _ _ (x:[]) = [x]
 getRandomSublistOfSize k seed xs = head (randomisedList) : getRandomSublistOfSize (k-1) (seed+1) (tail randomisedList)
-                                where
-                                    randomisedList = randomiseList seed xs
+                                where randomisedList = randomiseList seed xs
 
 generateChildren :: (Heuristic, Heuristic) -> HeuristicPopulation
 generateChildren (p1, p2) = [(c1, (0,0)), (c2, (0,0))]
-                            where
-                              (c1, c2) = kPointCrossover (p1, p2) 3 (getSeed p1)
+                            where (c1, c2) = kPointCrossover (p1, p2) 3 (getSeed p1)
 
 kPointCrossover :: (Heuristic, Heuristic) -> Int -> Int -> (Heuristic, Heuristic)
 kPointCrossover x 0 _ = x
@@ -73,23 +67,16 @@ kPointCrossover x k seed | (k `mod` 2 == 0) = kPointCrossover x (k-1) (seed+1)
 kPointCrossover x k seed | otherwise = onePointCrossover (kPointCrossover x (k-1) (seed+1)) seed
 
 onePointCrossover :: (Heuristic, Heuristic) -> Int -> (Heuristic, Heuristic)
-onePointCrossover (h1, h2) seed = (
+onePointCrossover (h1, h2) seed =
+                                  (
                                     (take i h1 ++ drop i h2),
                                     (take i h2 ++ drop i h1)
-                                  ) where
-                                    i = getRandomIndex seed h1
+                                  )
+                                  where i = getRandomIndex seed h1
 
 -- MUTATION
 mutationStep :: HeuristicPopulation -> HeuristicPopulation
--- mutationStep hPop = mutateHeuristics (selectHeuristicsToMutate (((getLargestCountOfEquivalents (map fst hPop)) `div` 2)+1) hPop)
 mutationStep hPop = mutateHeuristics (selectHeuristicsToMutate 2 hPop)
-
--- Gets the size of the largest set of equal values in a list
--- For example
--- ["11111111", "11111111", "11111111", "11110000", "11110000", "11111111", "00000000", "11110000"]
--- returns 4
-getLargestCountOfEquivalents :: Eq a => Ord a => [a] -> Int
-getLargestCountOfEquivalents xs = head (reverse (sort (map length (map (\y -> filter (==y) xs) (sortUniq xs)))))
 
 selectHeuristicsToMutate :: Int -> HeuristicPopulation -> [Heuristic]
 selectHeuristicsToMutate i [] = []
